7a941f3b5db53e0afed94a9051c24d580dcda2bd
Conan Torres <conantorreswf@gmail.com>
2023-08-11 15:51:21 -0300

    ovo ok

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function Calcular() {
     const peso = document.getElementById("peso").value;
     const proteina = (peso * 0.13).toFixed(2).replace(".", ",") + "g";
     const carbo = (peso * 0.011).toFixed(2).replace(".", ",") + "g";
-    const proteina_r = document.getElementById("proteina_r");
-    const carbo_r = document.getElementById("carbo_r");
+    const proteina_ovo = document.getElementById("proteina_ovo");
+    const carbo_ovo = document.getElementById("carbo_ovo");
 
     document.getElementById("result").classList.remove("nao-ativo");
 
-    proteina_r.textContent = proteina;
-    carbo_r.textContent = carbo;
+    proteina_ovo.textContent = proteina;
+    carbo_ovo.textContent = carbo;
   });
 }
 
