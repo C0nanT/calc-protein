5723b4e196e871fd4b2c170d0cc25791588656de
Conan Torres <conantorreswf@gmail.com>
2023-08-14 13:06:01 -0300

    pão de forma

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function Calcular_ovo() {
     event.preventDefault();
 
     const peso = document.getElementById("peso_ovo").value;
-    const proteina = (peso * 0.13).toFixed(2).replace(".", ",") + "g";
-    const carbo = (peso * 0.011).toFixed(2).replace(".", ",") + "g";
+    const proteina = (peso * 0.12).toFixed(2).replace(".", ",") + "g";
+    const carbo = (peso * 0.016).toFixed(2).replace(".", ",") + "g";
     const proteina_ovo = document.querySelector(".proteina_ovo");
     const carbo_ovo = document.querySelector(".carbo_ovo");
 
@@ -22,8 +22,8 @@ function Calcular_frango() {
     event.preventDefault();
 
     const peso = document.getElementById("peso_frango").value;
-    const proteina = (peso * 0.27).toFixed(2).replace(".", ",") + "g";
-    const carbo = (peso * 0.003).toFixed(2).replace(".", ",") + "g";
+    const proteina = (peso * 0.075).toFixed(2).replace(".", ",") + "g";
+    const carbo = (peso * 0).toFixed(2).replace(".", ",") + "g";
     const proteina_frango = document.querySelector(".proteina_frango");
     const carbo_frango = document.querySelector(".carbo_frango");
 
@@ -34,6 +34,24 @@ function Calcular_frango() {
   });
 }
 // --------------------------------------------------------------------------------------------------------------------
+function Calcular_pao() {
+  const formulario = document.querySelector(".formulario_pao");
+  formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const peso = document.getElementById("peso_pao").value;
+    const proteina = (peso * 0.095).toFixed(2).replace(".", ",") + "g";
+    const carbo = (peso * 0.51).toFixed(2).replace(".", ",") + "g";
+    const proteina_pao = document.querySelector(".proteina_pao");
+    const carbo_pao = document.querySelector(".carbo_pao");
+
+    document.querySelector(".result_pao").classList.remove("nao-ativo");
+
+    proteina_pao.textContent = proteina;
+    carbo_pao.textContent = carbo;
+  });
+}
 
 Calcular_frango();
 Calcular_ovo();
+Calcular_pao();
